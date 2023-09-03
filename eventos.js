9eee7ce26dc63dd56f155175ee653499f3080a06
sebastianfontenla <fontenlasebastian95@gmail.com>
2023-09-03 15:28:26 -0300

    solucionclick

--- eventos.js
+++ eventos.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', init);
-
+let cont = "";
 function init() {
-
+    const btn = document.getElementById('btn');
     const div = document.getElementById('eldiv');
 
+    btn.addEventListener('click', () => cont = "saludar");
     div.addEventListener('click', saludar);
 }
 
 function saludar() {
-    alert('Hola soy el div!')
+    console.log(cont);
+    if (cont == "saludar") {
+        alert('Hola!');
+        cont = ""
+    } else {
+        alert('Hola soy el div!');
+    }
 }
